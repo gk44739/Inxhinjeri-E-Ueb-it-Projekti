9acab2c12d4bc69d6f23d21453278f68a84b3c7a
Gentrit Krasniqi <gk44739@ubt-uni.net>
2020-05-13 02:01:08 +0200

    Genti Commit

--- js/main.js
+++ js/main.js
@@ -2,11 +2,14 @@ document.getElementById('back__to__top').addEventListener("click", function (){
     window.scrollTo(0, 0); 
 });
 var ontop=document.getElementById('back__to__top');
+var prevScrollpos = window.pageYOffset;
 window.addEventListener('scroll', function() {
-    if(window.pageYOffset > 500){
+    var currentScrollPos = window.pageYOffset;
+    if(currentScrollPos > prevScrollpos){
         ontop.classList.add("on__top__fixed");
     }else{
         ontop.classList.remove("on__top__fixed");
     }
+    prevScrollpos = currentScrollPos;
 });
 
