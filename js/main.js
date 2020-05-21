413e3e5a0db8c5c913b1cd6e18fcd5820759c67c
Gentrit Krasniqi <gk44739@ubt-uni.net>
2020-05-21 19:58:45 +0200

    Sign Up Login Form

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
-document.getElementById('back__to__top').addEventListener("click", function (){ 
-    window.scrollTo(0,0); 
-});
+var back__to__top=document.getElementById('back__to__top');
+if(back__to__top){
+    back__to__top.addEventListener("click", function (){ 
+        window.scrollTo(0,0); 
+    });
+}
 var ontop=document.getElementById('back__to__top');
 var prevScrollpos = window.pageYOffset;
 window.addEventListener('scroll', function() {
@@ -11,5 +14,27 @@ window.addEventListener('scroll', function() {
         ontop.classList.remove("on__top__fixed");
     }
     prevScrollpos = currentScrollPos;
+    
 });
+
+var loginbtn=document.getElementById('login__rid');
+if(loginbtn){
+    loginbtn.addEventListener("click", function (){
+        document.getElementsByClassName('signup__description')[0].style.display='none';
+        document.getElementsByClassName('signup__form')[0].style.display='none';
+        document.getElementsByClassName('login__description')[0].style.display='flex';
+        document.getElementsByClassName('login__form')[0].style.display='flex';
+    });
+}
+
+var signupbtn=document.getElementById('signup__rid');
+if(signupbtn){
+    signupbtn.addEventListener("click", function (){
+        document.getElementsByClassName('login__description')[0].style.display='none';
+        document.getElementsByClassName('login__form')[0].style.display='none';
+        document.getElementsByClassName('signup__description')[0].style.display='flex';
+        document.getElementsByClassName('signup__form')[0].style.display='flex';
+
+    });
+}
 
