97408c791a5274dea0c0c97d29813717300273c9
Gentrit Krasniqi <gk44739@ubt-uni.net>
2020-05-24 23:24:35 +0200

    Gentrit Front Dorzimi

--- js/main.js
+++ js/main.js
@@ -1,15 +1,3 @@
-
-var sticky__header=document.getElementById('header__nav');
-if(sticky__header){
-var sticky = sticky__header.offsetTop;
-    window.onscroll=function stickyHeader(){
-        if (window.pageYOffset > sticky) {
-            sticky__header.classList.add("sticky");
-        } else {
-            sticky__header.classList.remove("sticky");
-        }
-    }
-}
 var loginbtn=document.getElementById('login__rid');
 if(loginbtn){
     loginbtn.addEventListener("click", function (){
@@ -30,81 +18,95 @@ if(signupbtn){
 
     });
 }
-window.onload=function(){
-var slideTime=setInterval(autoPlay,4000);
-var next__button=document.getElementById('next__button');
-if(next__button){
-    next__button.addEventListener("click",function(){
-        clearInterval(slideTime);
-        slideTime=setInterval(autoPlay,4000);
-        var index=0;
-        var home__slider__content=document.getElementsByClassName('home__slider__content');
-        for(var div of home__slider__content){
-            index++;
-            var next__div=div.nextElementSibling;
-            if(div.classList.contains('visible') && index<home__slider__content.length){
-                next__div.classList.add('visible');
-                div.classList.remove('visible');
-                break;
-            }
-            else if(div.classList.contains('visible') && index==home__slider__content.length){
-                home__slider__content[0].classList.add('visible');
-                div.classList.remove('visible');
-                break;
-            }
+
+var sticky__header=document.getElementById('header__nav');
+if(sticky__header){
+var sticky = sticky__header.offsetTop;
+    window.onscroll=function stickyHeader(){
+        if (window.pageYOffset > sticky) {
+            sticky__header.classList.add("sticky");
+        } else {
+            sticky__header.classList.remove("sticky");
         }
-    });
-    
+    }
 }
-var prev__button=document.getElementById('prev__button');
-if(prev__button){
-    prev__button.addEventListener("click",function(){
-        clearInterval(slideTime);
-        slideTime=setInterval(autoPlay,4000);
-        var index=0;
-        var home__slider__content=document.getElementsByClassName('home__slider__content');
-        var last__slide=home__slider__content[home__slider__content.length-1];
+
+window.onload=function(){
+    var slideTime=setInterval(autoPlay,4000);
+    var next__button=document.getElementById('next__button');
+    if(next__button){
+        next__button.addEventListener("click",function(){
+            clearInterval(slideTime);
+            slideTime=setInterval(autoPlay,4000);
+            var index=0;
+            var home__slider__content=document.getElementsByClassName('home__slider__content');
             for(var div of home__slider__content){
                 index++;
-                prev__div=div.previousElementSibling;
+                var next__div=div.nextElementSibling;
                 if(div.classList.contains('visible') && index<home__slider__content.length){
-                    prev__div=div.previousElementSibling;
-                    if(prev__div==null){
-                        last__slide.classList.add('visible');
-                        div.classList.remove('visible');
-                        break;
-                    }else{
-                        prev__div.classList.add('visible');
-                        div.classList.remove('visible');
-                        break;
-                    }
-                }else if(div.classList.contains('visible') && index==home__slider__content.length){
-                    prev__div.classList.add('visible');
+                    next__div.classList.add('visible');
+                    div.classList.remove('visible');
+                    break;
+                }
+                else if(div.classList.contains('visible') && index==home__slider__content.length){
+                    home__slider__content[0].classList.add('visible');
                     div.classList.remove('visible');
                     break;
                 }
             }
-    });
-}
-
-function autoPlay(){
-    var index=0;
-    var home__slider__content=document.getElementsByClassName('home__slider__content');
-    for(var div of home__slider__content){
-        index++;
-        if(div.classList.contains('visible') && index<home__slider__content.length){
-                var next__div=div.nextElementSibling;
-                next__div.classList.add('visible');
-                div.classList.remove('visible');
-                break;
-        }else if(div.classList.contains('visible') && index==home__slider__content.length){
-                home__slider__content[0].classList.add('visible');
-                div.classList.remove('visible');
-                break;
+        });
+        
+    }
+    var prev__button=document.getElementById('prev__button');
+    if(prev__button){
+        prev__button.addEventListener("click",function(){
+            clearInterval(slideTime);
+            slideTime=setInterval(autoPlay,4000);
+            var index=0;
+            var home__slider__content=document.getElementsByClassName('home__slider__content');
+            var last__slide=home__slider__content[home__slider__content.length-1];
+                for(var div of home__slider__content){
+                    index++;
+                    prev__div=div.previousElementSibling;
+                    if(div.classList.contains('visible') && index<home__slider__content.length){
+                        prev__div=div.previousElementSibling;
+                        if(prev__div==null){
+                            last__slide.classList.add('visible');
+                            div.classList.remove('visible');
+                            break;
+                        }else{
+                            prev__div.classList.add('visible');
+                            div.classList.remove('visible');
+                            break;
+                        }
+                    }else if(div.classList.contains('visible') && index==home__slider__content.length){
+                        prev__div.classList.add('visible');
+                        div.classList.remove('visible');
+                        break;
+                    }
+                }
+        });
+    }
+    
+    function autoPlay(){
+        var index=0;
+        var home__slider__content=document.getElementsByClassName('home__slider__content');
+        for(var div of home__slider__content){
+            index++;
+            if(div.classList.contains('visible') && index<home__slider__content.length){
+                    var next__div=div.nextElementSibling;
+                    next__div.classList.add('visible');
+                    div.classList.remove('visible');
+                    break;
+            }else if(div.classList.contains('visible') && index==home__slider__content.length){
+                    home__slider__content[0].classList.add('visible');
+                    div.classList.remove('visible');
+                    break;
+            }
         }
     }
 }
-}
+
 var shop_list=document.getElementsByClassName('shop__thumbnails__li');
 for (var index = 0; index < shop_list.length; index++) {
     const element = shop_list[index];
@@ -123,7 +125,6 @@ for (var index = 0; index < shop_list.length; index++) {
 
 var minus=document.getElementById('minus');
 var plus=document.getElementById('plus');
-
 if(minus){
     minus.addEventListener("click",function(){
         var inputval=document.getElementsByClassName('costum__input')[0].getElementsByTagName('input')[0];
@@ -141,12 +142,22 @@ if(plus){
     });
 
 }
+
+var to__products=document.getElementById('to__products');
+var to__products__arrow=document.getElementById('to__products__arrow')
+if(to__products__arrow){
+    to__products__arrow.addEventListener("click", function (){ 
+        to__products.scrollIntoView();
+    });
+}
+
 var back__to__top=document.getElementById('back__to__top');
 if(back__to__top){
     back__to__top.addEventListener("click", function (){ 
         window.scrollTo(0,0)
     });
 }
+
 var ontop=document.getElementById('back__to__top');
 var prevScrollpos = window.pageYOffset;
 window.addEventListener('scroll', function() {
