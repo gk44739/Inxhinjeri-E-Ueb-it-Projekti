bf01c2523d100db4bc414ea62b298b863f21f56e
Gentrit Krasniqi <gk44739@ubt-uni.net>
2020-07-13 19:00:25 +0200

    genti commit

--- Model/contact_form.php
+++ Model/contact_form.php
@@ -6,26 +6,34 @@ class Contact_Form{
     private $file;
     private $content;
 
-    public function __construct($email, $subject, $content){
-        $this->email = $email;
+    public function __construct($subject,$email,$file,$content){
         $this->subject = $subject;
+        $this->email = $email;
+        $this->file = $file;
         $this->content = $content;
     }
 
+    public function getSubject(){
+        return $this->subject;
+    }
     public function getEmail(){
         return $this->email;
     }
-    public function getSubject(){
-        return $this->subject;
+    public function getFile(){
+        return $this->file;
     }
     public function getContent(){
         return $this->content;
     }
+
+    public function setSubject($n){
+        $this->subject = $n;
+    }
     public function setEmail($n){
         $this->email = $n;
     }
-    public function setSubject($n){
-        $this->subject = $n;
+    public function setFile($n){
+        $this->file = $n;
     }
     public function setContent($n){
         $this->content = $n;
---
+++ Model/contact_form_repository.php
@@ -0,0 +1,23 @@
+<?php
+require('connection_db.php');
+require('contact_form.php');
+function create(Contact_Form $contact_form){
+    global $connection;
+    $query = "INSERT INTO `contact_form` (`subject`,`email`,`file`,`content`,`sentdate`)VALUES('".$contact_form->getSubject()."','".$contact_form->getEmail()."', '".$contact_form->getFile()."','".$contact_form->getContent()."','".date("Y-m-d H:i:s")."')";
+    $create = mysqli_query($connection, $query) or die(mysqli_error($connection));  
+    return $create;  
+}
+
+function delete($subject){
+    global $connection;
+    $checknews = mysqli_query($connection, "Select id from users where subject='$subject'");
+    $result = mysqli_num_rows($checknews);
+    if($result == 1){
+        $query = "DELETE FROM `reports` WHERE subject='subject'";
+        $delete = mysqli_query($connection,$query) or die(mysqli_error($connection)); 
+        return $delete;
+    }else{
+        return false;
+    }
+}
+?>
